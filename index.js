ab75746841604c0f6c099710b45c6464c50db1cf
Osama Najjar <osama.najjar.m@gmail.com>
2018-11-09 03:25:26 +0100

    set cli-controller

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-#!/usr/bin/env node
-const cacheboxCli = require("./src/cachebox-cli").cli;
+const version = require("./lib/version");
+
+// cli-controller will map the given input & flages with the right action
+const cliController = (input, flags, showHelp) => {
+  if (flags.help) {
+    showHelp();
+  }
+  if (flags.version) {
+    console.log(version);
+  }
+  if (
+    Object.keys(flags).length === 2 &&
+    Object.keys(flags)[0] === "version" &&
+    !flags.version &&
+    input.length === 0
+  ) {
+    showHelp();
+  }
+};
+
+module.exports = cliController;
